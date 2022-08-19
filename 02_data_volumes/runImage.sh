1c1c93b988faf67cf23753bd6589d88b9643deaf
Andrew Zang <andrewz020529@gmail.com>
2022-08-18 21:46:20 -0400

    volumes and bind mounts

--- 02_data_volumes/runImage.sh
+++ 02_data_volumes/runImage.sh
@@ -4,4 +4,6 @@ docker run -d -p 3000:80 \
           --name feedback-node \
           --rm \
           -v feedback:/app/feedback \
+          -v "/Users/andrewzang/programming/docker/02_data_volumes:/app" \
+          -v /app/node_modules \
           02-volume-intro:volume
