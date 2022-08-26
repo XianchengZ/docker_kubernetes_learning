e6ceb806aec51c5b3846d4c2725e5726cb4f0232
Andrew Zang <andrewz020529@gmail.com>
2022-08-26 14:43:56 -0400

    started with deployment

---
+++ 06_deployToEC2/dockerPush.sh
@@ -0,0 +1,3 @@
+docker tag node-dep-example:latest xianchengz/node-ec2-deploy-example
+
+docker push xianchengz/node-ec2-deploy-example
---
+++ 06_deployToEC2/runImage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d --rm --name node-dep -p 80:80 node-dep-example 
